54a80bc40fd146aee231170862fadd9611a4e1c2
Kamilly-rp <kamillyr284@gmail.com>
2025-07-02 16:05:31 -0300

    ajuste do env

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,9 +3,8 @@ import dayjs from 'dayjs';
 import {toast} from 'react-toastify';
 
 export const api = axios.create({
-     baseURL: `${import.meta.env.REACT_APP_API_URL}/api`,
+     baseURL: `${process.env.REACT_APP_API_URL}/api`,
 });
-console.log("API URL:", import.meta.env.REACT_APP_API_URL);
 
 
 export const getAllProperties = async() => {
