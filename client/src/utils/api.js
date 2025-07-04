9905ebf85dac87114f8d2adca55245c757819726
Kamilly-rp <kamillyr284@gmail.com>
2025-07-04 14:22:35 -0300

    config. para deploy frontend versel

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,46 +1,43 @@
 import axios from 'axios';
 import dayjs from 'dayjs';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
+
+const baseURL = import.meta.env.PROD
+  ? import.meta.env.VITE_API_URL_PROD
+  : import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-     baseURL: `${import.meta.env.VITE_API_URL}/api`,
-
+  baseURL: `${baseURL}/api`,
 });
 
+export const getAllProperties = async () => {
+  try {
+    const response = await api.get("/residency/allresd", {
+      timeout: 10 * 1000,
+    });
 
-export const getAllProperties = async() => {
-    try {
-
-        const response = await api.get("/residency/allresd", {
-            timeout: 10 * 1000,
-        });
-
-        if (response.status === 400 || response.status === 500)
-        {
-            throw response.data
-        }
-        return response.data
-    }catch (error){
-        toast.error("Erro ao carregar os dados");
-        throw error 
+    if (response.status === 400 || response.status === 500) {
+      throw response.data;
     }
+    return response.data;
+  } catch (error) {
+    toast.error("Erro ao carregar os dados");
+    throw error;
+  }
 };
 
-export const getProperty = async( id) => {
-     try {
+export const getProperty = async (id) => {
+  try {
+    const response = await api.get(`/residency/${id}`, {
+      timeout: 10 * 1000,
+    });
 
-        const response = await api.get(`/residency/${id}`, {
-            timeout: 10 * 1000,
-        });
-
-        if (response.status === 400 || response.status === 500)
-        {
-            throw response.data
-        }
-        return response.data
-    }catch (error){
-        toast.error("Erro ao carregar os dados");
-        throw error 
+    if (response.status === 400 || response.status === 500) {
+      throw response.data;
     }
+    return response.data;
+  } catch (error) {
+    toast.error("Erro ao carregar os dados");
+    throw error;
+  }
 };
-
